d613ec40be5044ece731ffc06b5a297f531a1c7b
Sanghoonhan1334 <hsanghoon133334@gmail.com>
2025-09-06 23:03:56 +0900

    TypeScript 오류 수정: submitStatus 타입 캐스팅

--- src/app/inquiry/page.tsx
+++ src/app/inquiry/page.tsx
@@ -371,7 +371,7 @@ export default function InquiryPage() {
             )}
 
             {/* 성공 메시지 */}
-            {submitStatus === 'success' && (
+            {(submitStatus as string) === 'success' && (
               <Alert className="border-green-200 bg-green-50">
                 <CheckCircle className="h-4 w-4 text-green-600" />
                 <AlertDescription className="text-green-800">
