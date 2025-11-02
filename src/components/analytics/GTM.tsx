f961d3cda46db024b7a2f10c07e5e9135827dab7
Sanghoonhan1334 <hsanghoon133334@gmail.com>
2025-11-02 16:13:17 +0900

    GTM 스크립트 주입 방식 변경 - children 사용
    - dangerouslySetInnerHTML 제거
    - Script 컴포넌트의 children으로 직접 스크립트 주입
    - Next.js App Router에서 더 확실하게 작동하는 방식
    - 클라이언트에서 GTM 스크립트 확실히 실행되도록 개선

--- src/components/analytics/GTM.tsx
+++ src/components/analytics/GTM.tsx
@@ -9,23 +9,23 @@ export default function GTM() {
     return null
   }
 
-  // GTM 스크립트를 문자열로 생성
-  const gtmScript = `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${GTM_ID}');
-  `
-
   return (
     <>
       {/* Google Tag Manager */}
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{ __html: gtmScript }}
-      />
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       {/* Google Tag Manager (noscript) */}
       <noscript>
         <iframe
