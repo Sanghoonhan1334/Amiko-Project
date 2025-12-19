0c82d7b92a5449f7389ce5613c2210a61bdb835a
stella8006 <stella.j.moon@gmail.com>
2025-12-19 20:23:07 +0900

    feat: Set dark mode as default theme
    - Changed defaultTheme from 'light' to 'dark' in layout.tsx
    - Dark mode is now the default for all users
    - Users can still toggle to light mode if desired

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -144,7 +144,7 @@ export default function RootLayout({
         <QueryProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
+            defaultTheme="dark"
             enableSystem={false}
             disableTransitionOnChange={false}
           >
